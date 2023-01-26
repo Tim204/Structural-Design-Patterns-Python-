aa3586e3234a94da482c513e7b14c9a84501e363
Timoteo Tati <xfairybr@gmail.com>
2023-01-26 20:41:17 +0200

    composite

---
+++ composite pattern/composite.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+
+
+class Component(ABC):
+    @abstractmethod
+    def render(self):
+        pass
+
+    @abstractmethod
+    def move(self):
+        pass
+    pass
+
+
+class Shape(Component):
+
+    def move(self):
+        print("Move shape")
+
+    def render(self):
+        print("Render shape")
+
+
+class Group(Component):
+
+    def __init__(self):
+        self._components = []
+
+    def add(self, shape):
+        self._components.append(shape)
+
+    def render(self):
+        for component in self._components:
+            component.render()
+
+    def move(self):
+        for component in self._components:
+            component.move()
+
+
+group1 = Group()
+group1.add(Shape())  # Imaginary square shapes
+group1.add(Shape())
+
+group2 = Group()
+group2.add(Shape())  # Imaginary circle shapes
+group2.add(Shape())
+
+group = Group()
+group.add(group1)
+group.add(group2)
+group.render()
+group.move()
