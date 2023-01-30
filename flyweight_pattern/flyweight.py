efdcf597aee2f4bea096c21b4b4fdcd489652d61
Timoteo Tati <timoteo14@live.com.pt>
2023-01-30 09:20:16 +0200

    finilized flyweight

--- flyweight_pattern/flyweight.py
+++ flyweight_pattern/flyweight.py
@@ -8,11 +8,16 @@ class Point:
         self.icon = icon
 
     def draw(self):
-        print(f"{self.icon.name} at {self.x, self.y}")
+        print(f"{self.icon} at {self.x, self.y}")
 
 
 class PointIcon:
-    """The Flyweight"""
+    """
+       The Flyweight stores a common portion of the state (also called intrinsic
+       state) that belongs to multiple real business entities. The Flyweight
+       accepts the rest of the state (extrinsic state, unique for each entity) via
+       its method parameters.
+       """
     def __init__(self, type, icon):
         self.type = type
         self.icon = icon
@@ -20,10 +25,24 @@ class PointIcon:
     def get_type(self):
         return self.type
 
+    def __str__(self):
+        return str(self.type.name)
+
 
 class PointIconFactory:
+    """
+        The Flyweight Factory creates and manages the Flyweight objects. It ensures
+        that flyweights are shared correctly. When the client requests a flyweight,
+        the factory either returns an existing instance or creates a new one, if it
+        doesn't exist yet.
+    """
+    _icons = {}
+
     def get_point_icon(self, type):
-        icons = {PointType(type): PointIcon(type)}
+        if not self._icons.__contains__(type):
+            icon = PointIcon(type, None)
+            self._icons[type] = icon
+        return self._icons.get(type)
 
 class PointType(Enum):
     HOSPITAL = 1
@@ -33,16 +52,19 @@ class PointType(Enum):
 
 class PointService:
 
+    def __init__(self, icon_factory):
+        self._icon_factory = icon_factory
+
     def get_points(self):
         points = []
-        point = Point(1, 2, PointType.CAFE)
+        point = Point(1, 2, self._icon_factory.get_point_icon(PointType.CAFE))
         points.append(point)
 
         return points
 
 
 def client():
-    service = PointService()
+    service = PointService(PointIconFactory())
     for point in service.get_points():
         point.draw()
 
